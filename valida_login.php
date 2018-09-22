bd902af4a050c529fb77c8237ba7bf70806fc173
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-09-22 01:08:26 -0300

    Controle de usuários

--- valida_login.php
+++ valida_login.php
@@ -4,10 +4,16 @@
 
 
 	$usuario_autenticado = false;
+	$usuario_id  = null; 
+	$usuario_perfil_id = null;
+
+	$perfis = array(1 => 'administrativo', 2 => 'usuario');
 
 	$usuarios_app = array(
-		array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-		array('email' => 'user@teste.com.br', 'senha' => '123456')
+		array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+		array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2)
 	);
 
 	// echo '<pre>';
@@ -18,15 +24,18 @@
 		
 		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
 			$usuario_autenticado = true;
+			$usuario_id = $user['id'];
+			$usuario_perfil_id = $user['perfil_id'];
 		} 
 	}
 
 	if ($usuario_autenticado) {
 		echo 'Usuário autenticado com sucesso!';
 		$_SESSION['autenticado'] = 'SIM';
-		$_SESSION['valor1'] = 'X';
-		$_SESSION['valor3'] = 'Y';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_id'] = $usuario_perfil_id;
 		header('Location: home.php');
+
 	} else {
 		$_SESSION['autenticado'] = 'NAO';
 		header('Location: index.php?login=erro');
